54bb9e72173d4fc16b52a8e63c3439f9ebd25ed8
y-tsuzaki <>
2019-01-12 18:15:57 +0900

    format document

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,14 @@ const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
 const babel = require('gulp-babel');
 
-gulp.task("default", function() {
+gulp.task("default", function () {
   return gulp.src('src/sample_es6.js')
-      .pipe(babel({
-        "presets":["@babel/preset-env"]})) 
-      .pipe(uglify())
-      .pipe(rename({
-          extname: '.min.js'
-      }))
-      .pipe(gulp.dest('dest/'));
+    .pipe(babel({
+      "presets": ["@babel/preset-env"]
+    }))
+    .pipe(uglify())
+    .pipe(rename({
+      extname: '.min.js'
+    }))
+    .pipe(gulp.dest('dest/'));
 });
